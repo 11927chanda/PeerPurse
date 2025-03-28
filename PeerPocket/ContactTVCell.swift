1532d5520657ccee34931f30cec174383294181e
Chanda Shrestha <11927@ait.nsw.edu.au>
2025-03-28 16:01:29 +1100

    worked with repositories and UI for contact

---
+++ PeerPocket/ContactTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactTVCell.swift
+//  PeerPocket
+//
+//  Created by 11927 on 28/3/2025.
+//
+
+import UIKit
+
+class ContactTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
